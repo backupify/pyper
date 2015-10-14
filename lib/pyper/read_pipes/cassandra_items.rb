42b41f2fd2b538040effd7d477b9bb6319721faa
keith <keith@kwebber.backupify.org>
2015-10-14 17:19:48 -0400

    arguments

--- lib/pyper/read_pipes/cassandra_items.rb
+++ lib/pyper/read_pipes/cassandra_items.rb
@@ -15,7 +15,7 @@ module Pyper::ReadPipes
       page_size = arguments.delete(:page_size)
       paging_state = arguments.delete(:paging_state)
       order = arguments.delete(:order)
-      filtering = arugments.delete(:allow_filtering)
+      filtering = arguments.delete(:allow_filtering)
 
       opts = (options || {}).merge({ page_size: page_size, paging_state: paging_state})
 
