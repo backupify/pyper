aef4db325961b40cd49a988c90831ed6642e7e74
Chris Matysik <cmatysik@datto.com>
2016-07-14 14:42:38 -0400

    Refactoring test and pipe behavior

--- lib/pyper/pipes/default_values.rb
+++ lib/pyper/pipes/default_values.rb
@@ -2,22 +2,24 @@ module Pyper::Pipes
   # @param default_values [Hash] A hash of default values to set within the provided attrs if they are not already present.
   class DefaultValues < Struct.new(:default_values)
 
-    # @param attrs [Hash] The attributes of the item
+    # @param attrs_or_items [Hash] The attributes of the item
     # @param status [Hash] The mutable status field
     # @return [Hash] The item attributes with default values inserted
-    def pipe(attrs, status = {})
-      case attrs
-      when Hash then set_value(attrs)
-      when Enumerator::Lazy then set_value(attrs.to_h)
-      else attrs
+    def pipe(attrs_or_items, status = {})
+      case attrs_or_items
+      when Hash then set_value(attrs_or_items)
+      else attrs_or_items.map { |item| set_value(item) }
       end
     end
 
+    # @param item the item whose attributes will be set to the default values
+    # @return [Object] The object being modified
     def set_value(item)
+      new_item = item.dup
       default_values.each do |field, value|
-        item[field] = value
+        new_item[field] = value
       end
-      item
+      new_item
     end
   end
 end
--- test/unit/pyper/pipes/default_values_test.rb
+++ test/unit/pyper/pipes/default_values_test.rb
@@ -13,7 +13,7 @@ module Pyper::Pipes
       end
 
       should 'work if the passed in values are a in a lazy enumerator' do
-        assert_equal({ :a => 1, :b => 3, :c => 2 }, @pipe.pipe({ :a => 2, :b => 3, :c => nil}.lazy))
+        assert_equal([{ :a => 1, :b => nil, :c => 2 }, { :a => 1, :b => 3, :c => 2 }], @pipe.pipe([{ :a => 2, :b => nil, :c => 1 }, { :a => 1, :b => 3, :c => nil }].lazy).to_a)
       end
     end
   end
